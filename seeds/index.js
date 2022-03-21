c7c6c69579e80860d9f4f75d3fb98ded1e87af66
Ro Salgado <salgadorocio@hotmail.com>
2022-03-21 10:12:52 -0300

    adds author id

--- seeds/index.js
+++ seeds/index.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const Campground = require("../models/campground");
 const cities = require("./cities");
-const {descriptors, places} = require("./seedHelpers");
+const { descriptors, places } = require("./seedHelpers");
 mongoose
   .connect("mongodb://localhost:27017/yelp-camp", {
     useNewUrlParser: true,
@@ -15,27 +15,29 @@ mongoose
     console.log(err);
   });
 
-function getSample(array){
-    return array[Math.floor(Math.random()*array.length)];
+function getSample(array) {
+  return array[Math.floor(Math.random() * array.length)];
 }
 
 const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const randomCity = getSample(cities);
-    const price = Math.floor(Math.random()*20)+10;
+    const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
+      author: "62334d9c753f54b13deaa037",
       location: `${randomCity.city}, ${randomCity.state}`,
       title: `${getSample(descriptors)} ${getSample(places)}`,
       image: "https://source.unsplash.com/collection/483251",
-      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam quod eum error laborum mollitia exercitationem non ullam earum. Blanditiis aperiam, deserunt quibusdam iste cupiditate natus temporibus dolores aut similique dignissimos.",
-      price
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam quod eum error laborum mollitia exercitationem non ullam earum. Blanditiis aperiam, deserunt quibusdam iste cupiditate natus temporibus dolores aut similique dignissimos.",
+      price,
     });
 
     await camp.save();
   }
 };
 
-seedDB()
-.then(()=>{ mongoose.connection.close()})
-
+seedDB().then(() => {
+  mongoose.connection.close();
+});
