ffc1ea6fd3daa56939eba84d1beebb4dc5d40f67
Ro Salgado <salgadorocio@hotmail.com>
2022-03-21 10:01:50 -0300

    refactores routes code

--- app.js
+++ app.js
@@ -6,9 +6,13 @@ const session = require("express-session");
 const flash = require("connect-flash");
 const ExpressError = require("./utils/ExpressError");
 const methodOverride = require("method-override");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user");
 
 const campgroundRoutes = require("./routes/campgrounds");
 const reviewRoutes = require("./routes/reviews");
+const usersRoutes = require("./routes/users");
 
 mongoose
   .connect("mongodb://localhost:27017/yelp-camp", {
@@ -48,7 +52,15 @@ app.use(express.json());
 app.use(session(sessionConfig));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
   next();
@@ -56,6 +68,7 @@ app.use((req, res, next) => {
 
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:idCamp/reviews", reviewRoutes);
+app.use("/", usersRoutes);
 
 app.get("/", (req, res) => {
   res.render("home");
